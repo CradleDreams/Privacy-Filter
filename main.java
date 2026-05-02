48cc40530fb3e826784ff77ca683efaffd5df606
SEDOYJUNIOUR <zks2003@mail.ru>
2026-05-02 14:27:03 +0300

    add check phone numbers

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class main {
+    public static void main(String[] args) {
+        String inputFilePath = "input_letter.txt";
+        try {
+            String text = new String(Files.readAllBytes(Paths.get(inputFilePath)));
+
+            System.out.println("--- Исходный текст ---");
+            System.out.println(text);
+
+            String phoneRegex = "(\\+?\\d{1,3}[-.\\s]?)?\\(?\\d{3}\\)?[-.\\s]?\\d{3}[-.\\s]?\\d{2}[-.\\s]?\\d{2}";
+
+            String censoredText = text.replaceAll(phoneRegex, "[censored]");
+
+            System.out.println("\n--- Очищенный текст ---");
+            System.out.println(censoredText);
+        } catch (IOException e) {
+            System.err.println("Ошибка чтения файла: " + e.getMessage());
+        }
+    }
+}
